046fa961490b1953bb99430d7ed9cce078e40a32
AntipyukSemen <110487377+AntipyukSemen@users.noreply.github.com>
2023-04-15 15:46:51 +0300

    fix-3

--- js/for-loop-1.js
+++ js/for-loop-1.js
@@ -1,5 +1,5 @@
 const number = 7;
-for (let i = 7; i <= 7; i += 1) {
+for (let i = 1; i <= 10; i += 1) {
     const amount = number * i;
-    console.log(amount);
+    console.log(`${i} помножити на ${number} = ${amount}`);
 };
--- js/for-loop-3.js
+++ js/for-loop-3.js
@@ -1,9 +1,9 @@
-let amount = 0;
+let amount = 1;
 for (let i = 3; i <= 9; i += 1) {
-  if (i % 2 === 0) {
-    console.log("число парне");
-  } else {
-    amount = i * i;
+  if (i % 2 !== 0) {
+    amount *= i;
     console.log(amount);
+  } else {
+    console.log(`число ${i} парне`);
   };
 };
