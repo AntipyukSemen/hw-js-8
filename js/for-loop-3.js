4025373ca36d6d7b5be1c3aab86f0d04025aec39
AntipyukSemen <110487377+AntipyukSemen@users.noreply.github.com>
2023-04-15 15:21:39 +0300

    fix

--- js/for-loop-3.js
+++ js/for-loop-3.js
@@ -1,9 +1,9 @@
-// let amount;
-// for (let i = 3; i <= 9; i += 1) {
-//   if (i % 2 === 0) {
-//     console.log("число парне");
-//   } else {
-//     amount = i * i;
-//     console.log(amount);
-//   };
-// };
+let amount;
+for (let i = 3; i <= 9; i += 1) {
+  if (i % 2 === 0) {
+    console.log("число парне");
+  } else {
+    amount = i * i;
+    console.log(amount);
+  };
+};
--- js/for-loop-4.js
+++ js/for-loop-4.js
@@ -1,6 +1,6 @@
-// for (let i = 1; i <= 100; i += 1) {
-//     console.log(i);
-//     if (i === 49) {
-//         break;
-//     };
-// };
+for (let i = 1; i <= 100; i += 1) {
+    console.log(i);
+    if (i === 49) {
+        break;
+    };
+};
