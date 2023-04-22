e2799515d089c4863ab0697ecae2cd47baf0be82
AntipyukSemen <110487377+AntipyukSemen@users.noreply.github.com>
2023-04-22 16:00:35 +0300

    fix 3

--- js/for-loop-7.js
+++ js/for-loop-7.js
@@ -1,6 +1,15 @@
-// let number = 0;
+let number = 0;
 
 // while (number < 5) {
 //     number++;
 //     console.log(`число ${number}`);
 // };
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log(`число ${i}`);
+// };
+
+// do {
+//     number++;
+//     console.log(`число ${number}`);
+// } while(number < 5);
--- js/for-loop-8.js
+++ js/for-loop-8.js
@@ -1,10 +1,10 @@
-let grade = Number(prompt('Введіть вашу оцінку'));
-console.log(grade);
+// let grade = Number(prompt('Введіть вашу оцінку'));
+// console.log(grade);
 
-for (let i = grade; i < 7; i++) {
-  console.log(`У вас низький рівень знань ваш середній бал складає ${grade}`);
-};
+// for (let i = grade; i < 7; i++) {
+//   console.log(`У вас низький рівень знань ваш середній бал складає ${grade}`);
+// };
 
-for (let i = grade; i <= 12; i++) {
-  console.log(`У вас гарний рівень знань ваша оцінка ${grade}`);
-};
+// for (let i = grade; i <= 12; i++) {
+//   console.log(`У вас гарний рівень знань ваша оцінка ${grade}`);
+// };
